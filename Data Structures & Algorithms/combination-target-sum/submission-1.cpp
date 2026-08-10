59bd3d01bf126440e9fc33740511c33a47ed44ec
f202406guy-boop <f20240603@pilani.bits-pilani.ac.in>
2026-08-10 19:02:04 +0530

    Update: combination-target-sum - submission-1

--- Data Structures & Algorithms/combination-target-sum/submission-1.cpp
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int num=1;
-        int n=pow(2,nums.size());
-        string s;
-        vector<vector<int>> ans;
-        ans.push_back({});
-        for(int i=1;i<n;i++)
-        {
-            vector<int> temp;
-           s=bitset<32>(num).to_string();
-           for(int j=s.size()-1;j>=0;j--)
-           {
-            if(s[j]=='1') temp.push_back(nums[s.size()-j-1]);
-           }
-           num++;
-           ans.push_back(temp);
+    vector<vector<int>> ret;
+
+    void dp(int x, vector<int> &nums, vector<int> &temp, int idx) {
+        if(x == 0) {
+            ret.push_back(temp);
+            return;
         }
-        return ans;
+
+        for(int i = idx; i < nums.size(); i++) {   
+            if(x - nums[i] >= 0) {
+                temp.push_back(nums[i]);
+
+                dp(x - nums[i], nums, temp, i);   
+
+                temp.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> temp;
+        dp(target, nums, temp, 0);   
+        return ret;
     }
 };
